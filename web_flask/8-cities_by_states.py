667e128ff865b797f4e5b24536ca2ae6077841ac
tatsOre <tatianaorezap@gmail.com>
2020-09-01 17:43:37 -0500

    [Display Cities List]

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" 7-states_list.py
-Module that starts a Flask web application
-and lists all State instances from a database
+""" 8-cities_by_states.py
+Module that starts a Flask web application and lists
+all States and cities instances from a database
 """
 from flask import Flask, render_template
 from models.state import State
 from models import storage
+from os import getenv
 app = Flask(__name__)
 
 
@@ -15,11 +16,11 @@ def shutdown_session(response_or_exc):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_states_list():
+@app.route('/cities_by_states', strict_slashes=False)
+def display_cities():
     """Displays HTML page with list of states"""
     states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == "__main__":
